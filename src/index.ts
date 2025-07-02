8a3d18c90d2f5c8c790d62fc55ee922d9530a00d
Ren Koya <koya.ren.701@gmail.com>
2025-07-02 09:55:54 +0900

    export type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export * from "./client";
+
+export * from "./type/general/country";
+export * from "./type/general/exportColumns";
+export * from "./type/general/database";
